269b749e02acb05cebb412d645a28114361442ae
gustavo <andrescabral.c@gmail.com>
2016-09-12 00:31:38 -0400

    GET, POST and DELETE handlers bound to LevelDB operations.

--- levelup.js
+++ levelup.js
@@ -14,24 +14,6 @@ app.set('port', process.env.PORT || 3000);
 app.use(bodyParser.json());
 
 var db = levelup('./contact', {valueEncoding: 'json'});
-db.put('+359777123456', {
-  "firstName": "Joe",
-  "lastName": "Smith",
-  "title": "Mr.",
-  "company": "Dev Inc.",
-  "jobTitle": "Developer",
-  "primaryContactNumber": "+359777123456",
-  "otherContactNumbers": [
-    "+359777456789",
-    "+359777112233"
-  ],
-  "primaryEmailAddress": "joe.smith@xyz.com",
-  "emailAddresses": ["j.smith@xyz.com"],
-  "groups": [
-    "Dev",
-    "Family"
-  ]
-});
 
 app.get('/contacts/:number', function (request, response) {
   db.get(request.params.number, function (error, data) {
@@ -43,6 +25,55 @@ app.get('/contacts/:number', function (request, response) {
       response.send(data);
     }
   });
+});
+
+app.post('/contacts/:number', function (request, response) {
+  db.put(request.params.number, request.body, function (error) {
+    if (error) {
+      response.writeHead(500, {'Content-Type' : 'text/plain'});
+      response.end('Internal server error');
+    } else {
+      response.send(request.params.number + ' successfully inserted');
+    }
+  });
+});
+
+app.del('/contacts/:number', function (request, response) {
+  db.del(request.params.number, function (error) {
+    if (error) {
+      response.writeHead(500, {'Content-Type' : 'text/plain'});
+      response.end('Internal server error');
+    } else {
+      response.send(request.params.number + ' successfully deleted');
+    }
+  });
+});
+
+app.get('/contacts', function (request, response) {
+  var isFirst = true;
+  response.setHeader('content-type', 'application/json');
+
+  db.createReadStream()
+    .on('data', function (data) {
+      if (isFirst == true) {
+        response.write('[');
+      } else {
+        response.write(',');
+      }
+
+      response.write(JSON.stringify(data.value));
+      isFirst = false;
+    })
+    .on('error', function (error) {
+      console.log('Error while reading', error)
+    })
+    .on('close', function () {
+      console.log('Closing dbstream');
+    })
+    .on('end', function () {
+      console.log('Db stream closed');
+      response.end(']');
+    })
 });
 
 http.createServer(app).listen(app.get('port'));
